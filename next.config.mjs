fa612d3fd89aecf80ace6b6c1ecb7cefcff0b348
Dassi Dieudonne <dascomsoft@gmail.com>
2026-02-17 18:55:22 +0100

    updating the footer

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,39 @@
 
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['images.unsplash.com', 'via.placeholder.com'], // Ajoutez vos domaines d'images
+//   },
+// }
+
+// export default nextConfig; // <- Changement ici : export default au lieu de module.exports
+
+
+
+
+
+
+
+
+
+
+
+
+
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com'], // Ajoutez vos domaines d'images
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
-}
+  turbopack: {
+    // Indique explicitement à Turbopack le répertoire racine de votre projet
+    root: process.cwd(),
+  },
+};
 
-export default nextConfig; // <- Changement ici : export default au lieu de module.exports
+export default nextConfig;
